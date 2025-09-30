d2cc07b7866d0683ec59c5b849cd76c08e1ae6e8
Tomasz Drwięga <tomusdrw@gmail.com>
2025-09-30 16:41:50 +0200

    NPM @next works as well.

---
+++ tests/npm-next-works.test.ts
@@ -0,0 +1,12 @@
+import { describe, it } from "node:test";
+import { ExternalProcess } from "./external-process.js";
+
+const TEST_TIMEOUT = 10_000;
+
+describe("NPM published next version works", { timeout: TEST_TIMEOUT }, () => {
+  it("should display help", async () => {
+    const proc = ExternalProcess.spawn("npm/npx", "npx", "@typeberry/jam@next", "--help").terminateAfter(TEST_TIMEOUT);
+
+    await proc.waitForMessage(/typeberry\/jam.*by Fluffy Labs/);
+  });
+});
