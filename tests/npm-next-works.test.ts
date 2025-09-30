d3aa87b1cd53125d2f9552e64b7f2569c0be49ba
Tomasz Drwięga <tomusdrw@gmail.com>
2025-09-30 16:51:39 +0200

    Fix npm works.

--- tests/npm-next-works.test.ts
+++ tests/npm-next-works.test.ts
@@ -5,7 +5,9 @@ const TEST_TIMEOUT = 10_000;
 
 describe("NPM published next version works", { timeout: TEST_TIMEOUT }, () => {
   it("should display help", async () => {
-    const proc = ExternalProcess.spawn("npm/npx", "npx", "@typeberry/jam@next", "--help").terminateAfter(TEST_TIMEOUT);
+    const proc = ExternalProcess.spawn("npm/npx", "npx", "-y", "@typeberry/jam@next", "--help").terminateAfter(
+      TEST_TIMEOUT,
+    );
 
     await proc.waitForMessage(/typeberry\/jam.*by Fluffy Labs/);
   });
--- tests/npm-works.test.ts
+++ tests/npm-works.test.ts
@@ -5,7 +5,9 @@ const TEST_TIMEOUT = 10_000;
 
 describe("NPM published version works", { timeout: TEST_TIMEOUT }, () => {
   it("should display help", async () => {
-    const proc = ExternalProcess.spawn("npm/npx", "npx", "@typeberry/jam", "--help").terminateAfter(TEST_TIMEOUT);
+    const proc = ExternalProcess.spawn("npm/npx", "npx", "-y", "@typeberry/jam@latest", "--help").terminateAfter(
+      TEST_TIMEOUT,
+    );
 
     await proc.waitForMessage(/typeberry\/jam.*by Fluffy Labs/);
   });
