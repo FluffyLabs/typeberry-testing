f2f00ffd6f7609cbb4e3d385c3184962eb82f858
Tomasz Drwięga <tomusdrw@gmail.com>
2025-10-10 09:46:38 +0200

    Fix tsx.

--- picofuzz-benchmark/setup.sh
+++ picofuzz-benchmark/setup.sh
@@ -5,6 +5,10 @@ set -eu
 cd "$(dirname "${BASH_SOURCE[0]}")"
 cd ..
 
+# for some reason it's not available sometimes
+echo "Make sure tsx is installed"
+npm i tsx
+
 echo "==> Building picofuzz..."
 npm run build-docker -w @fluffylabs/picofuzz
 
